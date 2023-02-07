6252b83704b63800e54bb13bf249a8c47dbe65ec
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-06 22:01:10 -0700

    added data info for current weather

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -9,22 +9,27 @@ $(".btn").on("click", function (event) {
     event.preventDefault();
     var city = $("input").val();
     var overview = $(".city-basic");
+    var currentTemp = $("#current-temp");
     var memory = $(".memory");
 
     var WeatherAPI = "1453cb68cafdfe7161851616395bc88b";
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + WeatherAPI;
-    fetch(queryURL)
+    fetch(queryURL).then(res => {
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            var cityName = $("#city-name");
+            var currentTemp = $("#current-temp");
+            var wind = $("#wind");
+            var humidity = $("#humidity");
+            cityName.text(data.name)
+            currentTemp.text(data.main.temp + "")
+            wind.text(data.wind.speed + " mph")
+            humidity.text(data.main.humidity + "%")
+        })
+        .catch(error => console.log('ERROR'))
 
-    .then(function (response) {
-        return response.json();
-    })
-
-    .then(function (weatherRes) {
-        overview.textContent = "";
-        console.log(weatherRes);
-        
-    })
-    
     localStorage.setItem("recent", city);
     var recent = localStorage.getItem("recent")
     memory.textContent = recent;
